f16c3818b9fe4967eea432476f0dfc7b71f9fbfe
Mika Leiman <ext-mika.1.leiman@nokia.com>
2011-01-12 13:10:41 +0200

    Updated polling interval and added testabilitydriver@nokia.com mail address.

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,7 +2,7 @@
 
 Project.configure do |project|
  
-  project.email_notifier.emails = ['ext-mika.1.leiman@nokia.com']
+  project.email_notifier.emails = ['ext-mika.1.leiman@nokia.com','testabilitydriver@nokia.com']
   
   project.email_notifier.from = 'ext-mika.1.leiman@nokia.com'
 
@@ -15,6 +15,6 @@ Project.configure do |project|
   # project.build_command = 'build_my_app.sh'
 
   # Ping Subversion for new revisions every 5 minutes (default: 30 seconds)
-  # project.scheduler.polling_interval = 5.minutes
+  project.scheduler.polling_interval = 5.minutes
 
 end
