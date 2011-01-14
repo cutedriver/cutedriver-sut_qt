649390525b8469f6f4ca9e048da9ff6c2ebbf733
Jussi Koljonen <ext-jussi.koljonen@nokia.com>
2011-01-14 11:50:22 +0200

    sut_qt#plugin.rb: include TDriverVerify

--- lib/testability-driver-plugins/testability-driver-qt-sut-plugin/plugin.rb
+++ lib/testability-driver-plugins/testability-driver-qt-sut-plugin/plugin.rb
@@ -20,6 +20,8 @@
 # verify that TDriver is loaded
 Kernel::raise RuntimeError.new( "This SUT plugin requires Testability Driver and cannot be launched in standalone mode" ) unless (defined?( MATTI ) || defined?( TDriver ))
 
+include TDriverVerify
+
 module MobyPlugin
 
 	module QT
