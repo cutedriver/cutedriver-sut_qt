ed0bccefe0c654f0c0919e4e9eadebd4d3c07265
Tuukka Lahtela <ext-tuukka.m.lahtela@nokia.com>
2011-10-24 11:05:08 +0300

    added 5 pixels to fix two line links

--- lib/testability-driver-plugins/testability-driver-qt-sut-plugin/behaviours/webkit.rb
+++ lib/testability-driver-plugins/testability-driver-qt-sut-plugin/behaviours/webkit.rb
@@ -19,7 +19,7 @@
 
 module MobyBehaviour
 
-	module QT
+  module QT
 
     # == description
     # Webkit specific behaviours
@@ -42,88 +42,88 @@ module MobyBehaviour
     # == objects
     # *
     #
-		module Webkit
+    module Webkit
 
-			include MobyBehaviour::QT::Behaviour
+      include MobyBehaviour::QT::Behaviour
 
-            # == nodoc
-            # == description
-            # Send javascript to target object on the screen
-            # == params
-            # java_script::( )java script to be executed to target object
-            # == returns  
-            # == raises
-            # TestObjectNotFoundError:: If a graphics item is not visible on screen
-            # ArgumentError:: If an invalid button type is given
-            # ArgumentError:: If coordinates are outside of the object
-            # === examples
-            #  @object.execute_javascript_query
-            def execute_javascript_query(java_script, locator_query = nil, index = -1, webframe_id = 0)   
+      # == nodoc
+      # == description
+      # Send javascript to target object on the screen
+      # == params
+      # java_script::( )java script to be executed to target object
+      # == returns  
+      # == raises
+      # TestObjectNotFoundError:: If a graphics item is not visible on screen
+      # ArgumentError:: If an invalid button type is given
+      # ArgumentError:: If coordinates are outside of the object
+      # === examples
+      #  @object.execute_javascript_query
+      def execute_javascript_query(java_script, locator_query = nil, index = -1, webframe_id = 0)   
 
-              returnValue = ""
-              
-                begin   
-                  
-                  command = command_params #in qt_behaviour           
+        returnValue = ""
+        
+        begin   
+          
+          command = command_params #in qt_behaviour           
 =begin
-                  if(locator_query == nil)
-                     command.command_name('ExecuteJavaScriptOnQWebFrame')
-                     locator_query=""
-                  else  
-                     command.command_name('ExecuteJavaScriptOnWebElement')
-                  end
+             if(locator_query == nil)
+               command.command_name('ExecuteJavaScriptOnQWebFrame')
+               locator_query=""
+             else  
+               command.command_name('ExecuteJavaScriptOnWebElement')
+             end
 =end                  
 
-                  if type != "QWebFrame"
-                  command.command_name('ExecuteJavaScriptOnWebElement')
-                  webframe_id = attribute('webFrame') if webframe_id.to_s == "0"				  
-                  else
-                  command.command_name('ExecuteJavaScriptOnQWebFrame')
-                  end
+          if type != "QWebFrame"
+            command.command_name('ExecuteJavaScriptOnWebElement')
+            webframe_id = attribute('webFrame') if webframe_id.to_s == "0"				  
+          else
+            command.command_name('ExecuteJavaScriptOnQWebFrame')
+          end
 
-                  command.service( 'webkitCommand' )
-                  params = {
-                    'java_script'   => java_script,
-                    'locator_query' => locator_query,
-                    'index'         => index.to_s,
-                    'elementId'     => @id.to_s, 
-                    'webframe_id'   => webframe_id.to_s
-                  }
-                  
-                  command.command_params( params )
-                  
-                  returnValue = @sut.execute_command( command )
+          command.service( 'webkitCommand' )
+          params = {
+            'java_script'   => java_script,
+            'locator_query' => locator_query,
+            'index'         => index.to_s,
+            'elementId'     => @id.to_s, 
+            'webframe_id'   => webframe_id.to_s
+          }
           
-                rescue Exception => e      
+          command.command_params( params )
+          
+          returnValue = @sut.execute_command( command )
+          
+        rescue Exception => e      
 
-                    $logger.behaviour "FAIL;Failed send javascript with execute_javascript \"#{java_script}\""
-                    raise e        
+          $logger.behaviour "FAIL;Failed send javascript with execute_javascript \"#{java_script}\""
+          raise e        
 
-                end      
+        end      
 
-                $logger.behaviour "PASS;Operation send javascript executed successfully with execute_javascript \"#{java_script}\""
+        $logger.behaviour "PASS;Operation send javascript executed successfully with execute_javascript \"#{java_script}\""
 
-              returnValue
+        returnValue
 
-            end
-            
+      end
+      
       # == description
-			# Executes java script on the target test object			
-			#
-			# == arguments
-			# java_script
-			#  String
-			#   description: Script to be executed to target object
-			#   example: "this.onclick();"
-			#
-			# == returns
-			# MobyBase::TestObject
-			#  description: Target test object 
-			#  example: -
-			#
-			# == raises
-			# RuntimeError
-			#  description: When executing JavaScript to QWebFrame: QWebPage not found.
+      # Executes java script on the target test object			
+      #
+      # == arguments
+      # java_script
+      #  String
+      #   description: Script to be executed to target object
+      #   example: "this.onclick();"
+      #
+      # == returns
+      # MobyBase::TestObject
+      #  description: Target test object 
+      #  example: -
+      #
+      # == raises
+      # RuntimeError
+      #  description: When executing JavaScript to QWebFrame: QWebPage not found.
       #
       # RuntimeError
       #  description: When executing JavaScript to QWebFrame: QWebFrame not found.
@@ -134,63 +134,63 @@ module MobyBehaviour
       # RuntimeError
       #  description: When executing JavaScript to WebElement: QWebElement not found.
       #
-			def execute_javascript( java_script )   
-                
-              webframe_id = 0
-              query = ""
-              index = -1
-			  if type == "QWebFrame"
-			      query = nil
-              else
-                  query << self.type.to_s 
-    			  creation_attributes.each {|param, value|
-    			    if param.to_s == "__index"
-    			      index=value.to_i
-    			      next
-    			    end 
-                    query << "[" << param.to_s << "='" << value.to_s << "'" << "]"
-    			  }
-    		  end
-    		  
-    		  if type != "QWebFrame" #&& parent_object.type == "QWebFrame")
-    		    #webframe_id = parent_object.id
-				webframe_id = attribute('webFrame')
-    		  end
+      def execute_javascript( java_script )   
+        
+        webframe_id = 0
+        query = ""
+        index = -1
+        if type == "QWebFrame"
+          query = nil
+        else
+          query << self.type.to_s 
+          creation_attributes.each {|param, value|
+            if param.to_s == "__index"
+              index=value.to_i
+              next
+            end 
+            query << "[" << param.to_s << "='" << value.to_s << "'" << "]"
+          }
+        end
+        
+        if type != "QWebFrame" #&& parent_object.type == "QWebFrame")
+          #webframe_id = parent_object.id
+          webframe_id = attribute('webFrame')
+        end
 
-              execute_javascript_query( java_script, nil, index, webframe_id)
-			end
+        execute_javascript_query( java_script, nil, index, webframe_id)
+      end
 
       # == description
-			# Scroll QWebFrame to correct position. Valid target object types are: QWebFrame, all QWebElements.
-			#
-			# == arguments
-			# dx
-			#  Fixnum
-			#   description: Delta x in pixels, positive value to right negative to left. Ignored for QWebElements. 
-			#   example: 313
+      # Scroll QWebFrame to correct position. Valid target object types are: QWebFrame, all QWebElements.
       #
-			# dy
-			#  Fixnum
-			#   description: Delta y in pixels, positive value to down negative to up. Ignored for QWebElements.
-			#   example: 313
-			#
-			# center 
-			#  Fixnum
-			#   description: Value 1 means that scroll is done only to reveal the center position of the element. Value 0 means that element is tried to bring fully visible. Ignored for QWebFrame.
-			#   example: 1
-			#
-			# == returns
+      # == arguments
+      # dx
+      #  Fixnum
+      #   description: Delta x in pixels, positive value to right negative to left. Ignored for QWebElements. 
+      #   example: 313
+      #
+      # dy
+      #  Fixnum
+      #   description: Delta y in pixels, positive value to down negative to up. Ignored for QWebElements.
+      #   example: 313
+      #
+      # center 
+      #  Fixnum
+      #   description: Value 1 means that scroll is done only to reveal the center position of the element. Value 0 means that element is tried to bring fully visible. Ignored for QWebFrame.
+      #   example: 1
+      #
+      # == returns
       # NilClass
       #  description: -
       #  example: -
       #
-			# == exceptions
-			# RuntimeError
-			#  description: Scrollign webframe failed with error: %s
-			#
-			# === examples
-			#  @QwebFrame.scroll(0, 10)
-			#  @app.a.scroll()
+      # == exceptions
+      # RuntimeError
+      #  description: Scrollign webframe failed with error: %s
+      #
+      # === examples
+      #  @QwebFrame.scroll(0, 10)
+      #  @app.a.scroll()
       def scroll( dx = 0, dy = 0, center = 0 )   
         temp_id = ""
 
@@ -217,9 +217,9 @@ module MobyBehaviour
             
             #adjust image to be visible for tap
             if(center_y.to_i > frame_down_border)
-              dy = center_y.to_i - frame_down_border + 1
+              dy = center_y.to_i - frame_down_border + 5
             elsif (center_y.to_i < y_absolute) 
-              dy = center_y.to_i - y_absolute - 1
+              dy = center_y.to_i - y_absolute - 5
             end
             
             if(center_x.to_i > frame_right_border)
@@ -234,7 +234,7 @@ module MobyBehaviour
             elsif (center_y.to_i < y_absolute)
               dy = attribute("y_absolute").to_i - y_absolute
             end
-          
+            
             if(center_x.to_i > frame_right_border)
               dx = attribute("x_absolute").to_i + attribute("width").to_i - x_absolute - width + verticalScrollBarWidth
             elsif (center_x.to_i < x_absolute)
@@ -261,12 +261,12 @@ module MobyBehaviour
       end
 
 
-			# enable hooking for performance measurement & debug logging
-			TDriver::Hooking.hook_methods( self ) if defined?( TDriver::Hooking )
+      # enable hooking for performance measurement & debug logging
+      TDriver::Hooking.hook_methods( self ) if defined?( TDriver::Hooking )
 
 
-		end # Webkit
+    end # Webkit
 
-	end # QT
+  end # QT
 
 end # MobyBehaviour
